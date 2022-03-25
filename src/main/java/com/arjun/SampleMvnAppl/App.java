fb23071f77df449ef94e9f108994ffbd62eb93d9
Arjun <aj629280@gmail.com>
2022-03-25 18:35:55 +0530

    arjun

---
+++ src/main/java/com/arjun/SampleMvnAppl/App.java
@@ -0,0 +1,13 @@
+package com.arjun.SampleMvnAppl;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello Welcome From Maven App" );
+    }
+}
